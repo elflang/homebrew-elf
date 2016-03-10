8087bcb61fcbe9f73c0209a8d42b9f303d8966b4
L-Elf Karlstein <lelfjior@gmail.com>
2016-03-09 22:30:25 -0600

    Minor

--- elf.rb
+++ elf.rb
@@ -1,7 +1,7 @@
 class Elf < Formula
   desc "A self-hosted Lisp for Lua and JavaScript"
   homepage "https://github.com/elflang/elf"
-  url "https://raw.githubusercontent.com/elflang/homebrew-elf/master/release/elf-1.0.0.tar.gz"
+  url "https://raw.githubusercontent.com/elflang/homebrew-elf/master/release/elf-0.tar.gz"
   sha256 "e043cde06d390d3c65a55ed83572cbd10b24d02d9594243f921832143127e893"
 
   bottle :unneeded
